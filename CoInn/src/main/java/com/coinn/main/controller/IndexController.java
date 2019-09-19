47ceb59f7f276c3e8136879c5967803b10b766a5
jookim <joonho.kim@dhl.com;code.juno@gmail.com>
2019-09-19 10:32:42 +0900

    added html5 boilerplate files

---
+++ CoInn/src/main/java/com/coinn/main/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.coinn.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.java.Log;
+
+
+
+@Log
+@Controller
+@RequestMapping("/coinn/main")
+public class IndexController {
+
+	
+	
+	@GetMapping("/index")
+	public void index() {
+		
+	}
+}
