dd2aced705a05c7217ea59546d68511d3ec18510
jookim <joonho.kim@dhl.com;code.juno@gmail.com>
2019-09-19 10:06:25 +0900

    initial commit

---
+++ CoInn/src/main/java/com/coinn/CoInnApplication.java
@@ -0,0 +1,13 @@
+package com.coinn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoInnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoInnApplication.class, args);
+	}
+
+}
---
+++ CoInn/src/test/java/com/coinn/CoInnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coinn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoInnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
